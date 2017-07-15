d40f0b84cce05a171ad068337e4736a9dd243271
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-15 18:55:41 +0200

    Consuming API on server side using C#

--- Tours_API/WebFormClient.aspx.cs
+++ Tours_API/WebFormClient.aspx.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ExploreTanga.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,6 +13,20 @@ namespace Tours_API
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:13058");
+            var tour = new Tour()
+            {
+                Name = "Http Client Tour in class",
+                Description = "Dummy client description with Http Client",
+                Length = 21,
+                IncludesMeals = false,
+                Price = 210.0M,
+                Rating = null
+            };
+            client.PostAsJsonAsync("api/tours", tour);
+
 
         }
     }
