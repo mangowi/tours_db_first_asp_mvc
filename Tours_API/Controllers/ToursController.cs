9ff7620e26fb366108f5e4e54991ab4c642419c7
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-15 15:55:05 +0200

    Adding Web API controller using scaffold

---
+++ Tours_API/Controllers/ToursController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ExploreTanga.DAL;
+
+namespace Tours_API.Controllers
+{
+    public class ToursController : ApiController
+    {
+        private MyDbContext db = new MyDbContext();
+
+        // GET: api/Tours
+        public IQueryable<Tour> GetTours()
+        {
+            return db.Tours;
+        }
+
+        // GET: api/Tours/5
+        [ResponseType(typeof(Tour))]
+        public IHttpActionResult GetTour(int id)
+        {
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tour);
+        }
+
+        // PUT: api/Tours/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTour(int id, Tour tour)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tour.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tour).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TourExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Tours
+        [ResponseType(typeof(Tour))]
+        public IHttpActionResult PostTour(Tour tour)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Tours.Add(tour);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tour.Id }, tour);
+        }
+
+        // DELETE: api/Tours/5
+        [ResponseType(typeof(Tour))]
+        public IHttpActionResult DeleteTour(int id)
+        {
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return NotFound();
+            }
+
+            db.Tours.Remove(tour);
+            db.SaveChanges();
+
+            return Ok(tour);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TourExists(int id)
+        {
+            return db.Tours.Count(e => e.Id == id) > 0;
+        }
+    }
+}
