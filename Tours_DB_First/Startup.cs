94ded434654d077eef9732d1e838a8cbb70d47cb
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-15 19:15:15 +0200

    Establishing two communication using signalR that doesn't rely on request

---
+++ Tours_DB_First/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute (typeof(Tours_DB_First.Startup))]
+
+namespace Tours_DB_First
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
